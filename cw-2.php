5be8b81bd066e5f577d148fca11a1e0e559133d9
DonaldKellett <i.donaldl@me.com>
2016-05-19 18:59:48 +0800

    Add describe method

--- cw-2.php
+++ cw-2.php
@@ -1,3 +1,51 @@
 <?php
+try {
+  class Test {
+    /* Keeps track of the number of passes.  Cannot be modified externally. */
+    protected $passes = 0;
 
+    /* Keeps track of the number of fails.  Cannot be modified externally. */
+    protected $fails = 0;
+
+    /* Ensures a "describe" block is not nested in another.  Cannot be modified externally. */
+    protected $describing = false;
+
+    /* Describes the tests to be executed.  Wraps the test output in <DESCRIBE::> and <COMPLETEDIN::> */
+    public function describe($msg, $fn) {
+      /* If describing message is empty or not defined, set a default message */
+      if (!$msg) $msg = "The PHP code to be tested and executed";
+
+      /* Start timing the script */
+      $start = microtime(true);
+
+      try {
+        /* If the current describe block is nested in another describe block, halt the process before any testing occurs */
+        if ($this->describing) throw new Exception("cannot call describe within another describe");
+
+        /* Now that a describe block is being used, another describe block cannot be nested inside it */
+        $this->describing = true;
+
+        /* Format the "describe" block and run the tests */
+        echo "<DESCRIBE::>$msg\n";
+        $fn();
+      } catch (Exception $e) {
+        /* If an error exists, output it to the user */
+        echo "<ERROR::>$e\n";
+      } finally {
+        /* Time the script and round to the nearest millisecond.  Output that to the user. */
+        $dur = round((microtime(true) - $start) * 1000);
+        echo "<COMPLETEDIN::>$dur\n";
+
+        /* The describe block has ended.  Reset "describing" */
+        $this->describing = false;
+
+        /* If any tests failed inside the current "describe" block, throw an error to prevent further execution */
+        if ($this->fails > 0) throw new Exception("Failed Tests");
+      }
+    }
+  }
+  $test = new Test;
+} catch (Exception $e) {
+  throw new Exception("Failed to load core API methods");
+}
 ?>
--- testing.php
+++ testing.php
@@ -1,3 +1,6 @@
 <?php
 require 'cw-2.php';
+$test->describe("My custom tests", function () {
+  echo "Hello World";
+});
 ?>
